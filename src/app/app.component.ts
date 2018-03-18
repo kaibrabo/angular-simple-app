13a6a13d0cc036b4059fd04c118f01072671fe76
Kai Brabo <kaibrabo@gmail.com>
2018-03-18 02:10:07 -0700

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'A Simple App built with Angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// NgModel lives here
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //added
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
