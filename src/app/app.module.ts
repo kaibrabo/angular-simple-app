9874ad8c09735d68f8b2d40e65da22d0e23263e2
Kai Brabo <kaibrabo@gmail.com>
2018-03-21 01:26:16 -0700

    added hero service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service'
 
 
 @NgModule({
@@ -19,7 +20,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     //added
     FormsModule
   ],
-  providers: [],
+  providers: [
+      HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
